2c1f0589caf072e3bedfb3eb34f8e64b3a426c12
User <gustavoseveriano@alunos.utfpr.edu.br>
2025-01-21 21:03:40 -0300

    Projeto loja em Java (POO 2023)

---
+++ src/main/java/com/mycompany/projeto_consultoriomedico/Projeto_consultorioMedico.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_consultoriomedico;
+
+/**
+ *
+ * @author User
+ */
+public class Projeto_consultorioMedico {
+
+    public static void main(String[] args) {
+        
+       // TELA_LOGIN_ADM LoginADM = new TELA_LOGIN_ADM();
+       // LoginADM.setVisible(true);
+       
+       //TELA_CENTRAL_ADM TelaCentralADM = new TELA_CENTRAL_ADM();
+       //TelaCentralADM.setVisible(true);
+       
+       //TELA_ADM_VIEW_RECEPCIONISTA TelaViewRecepcionista = new TELA_ADM_VIEW_RECEPCIONISTA();
+       //TelaViewRecepcionista.setVisible(true);
+       
+       //TELA_ADM_VIEW_MEDICO TelaViewMedico = new TELA_ADM_VIEW_MEDICO();
+       //TelaViewMedico.setVisible(true);
+       
+        //TELA_ADM_VIEW_ADMINISTRADOR TelaViewADM = new TELA_ADM_VIEW_ADMINISTRADOR();
+        //TelaViewADM.setVisible(true);
+        
+        //TELA_ADM_SELECIONAR_OPCAO_FUNCIONARIO TelaSelectFunc = new TELA_ADM_SELECIONAR_OPCAO_FUNCIONARIO();
+        //TelaSelectFunc.setVisible(true);
+        
+        //TELA_ADM_RESULTADO_CONSULTA_RECEPCIONISTA_ADM TelaResultRecepAndADM = new TELA_ADM_RESULTADO_CONSULTA_RECEPCIONISTA_ADM();
+        //TelaResultRecepAndADM.setVisible(true);
+        
+        //TELA_ADM_RESULTADO_CONSULTA_MEDICO TelaResultMed = new TELA_ADM_RESULTADO_CONSULTA_MEDICO();
+        //TelaResultMed.setVisible(true);
+        
+        
+        //TELA_ADM_EXCLUIR_USUARIO TelaExcluirUser = new TELA_ADM_EXCLUIR_USUARIO();
+        //TelaExcluirUser.setVisible(true);
+        
+        //TELA_ADM_CONSULTAR_RECEPCIONISTA TelaConsultRecepcionista = new TELA_ADM_CONSULTAR_RECEPCIONISTA();
+        //TelaConsultRecepcionista.setVisible(true);
+        
+        //TELA_ADM_CONSULTAR_MEDICO TelaConsultMedico = new TELA_ADM_CONSULTAR_MEDICO();
+        //TelaConsultMedico.setVisible(true);
+        
+        //TELA_ADM_CONSULTAR_ADM TelaConsultADM = new TELA_ADM_CONSULTAR_ADM();
+        //TelaConsultADM.setVisible(true);
+        
+        //TELA_ADM_CONFIRMAR_EXCLUSAO_USUARIO TelaConfirmExclusao = new TELA_ADM_CONFIRMAR_EXCLUSAO_USUARIO();
+        //TelaConfirmExclusao.setVisible(true);
+        
+        //TELA_ADM_CONFIRMAR_ALTERACAO_USUARIOS TelaAlterarUsuario = new TELA_ADM_CONFIRMAR_ALTERACAO_USUARIOS();
+        //TelaAlterarUsuario.setVisible(true);
+        
+        //TELA_ADM_CADASTRAR_RECEPCIONISTA TelaCadastroRecepcionista = new TELA_ADM_CADASTRAR_RECEPCIONISTA();
+        //TelaCadastroRecepcionista.setVisible(true);
+        
+        //TELA_ADM_CADASTRAR_MEDICO TelaCadastroMedico = new TELA_ADM_CADASTRAR_MEDICO();
+        //TelaCadastroMedico.setVisible(true);
+        
+        //TELA_ADM_CADASTRAR_ADM TelaCadastroADM = new TELA_ADM_CADASTRAR_ADM();
+        //TelaCadastroADM.setVisible(true);
+        
+        //TELA_ADM_CADASTRAR_INFO_CONTATO TelaCadastrarInfoContato = new TELA_ADM_CADASTRAR_INFO_CONTATO();
+        //TelaCadastrarInfoContato.setVisible(true);
+        
+        //TELA_ADM_FINALIZAR_CADASTRO TelaFinalizarCad = new TELA_ADM_FINALIZAR_CADASTRO();
+        //TelaFinalizarCad.setVisible(true);
+        
+        //TELA_ADM_ALTERAR_USUARIOS TelaAlterarUsuarios = new TELA_ADM_ALTERAR_USUARIOS();
+        //TelaAlterarUsuarios.setVisible(true);
+        
+        TELA_ADM_ALTERAR_DADOS_CONTATO TelaAlterarDadosContato = new TELA_ADM_ALTERAR_DADOS_CONTATO();
+        TelaAlterarDadosContato.setVisible(true);
+        
+    }    
+}
